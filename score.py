f67b42784e99b67c4d8316bcbcd36216bc9347bb
admin <admin@users.noreply.github.com>
2022-06-28 20:10:22 +0800

    add space before +

--- score.py
+++ score.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     weights_bass = torch.Tensor([2.33, 2.53, 3.6, 3.4, 3.17, 4.23, 3.37])
     weight_mid = torch.Tensor([2.53, 2.63, 3.63, 3.27, 2.5, 3.67, 2.97])
     weight_treble = torch.Tensor([2.37, 2.97, 3.67, 3.2, 2.93, 4, 3.07])
-    weight_avg = (weights_bass + weight_mid+weight_treble) / 3
+    weight_avg = (weights_bass + weight_mid + weight_treble) / 3
 
     output = eval(tag='./test/KAWAI.wav',
                   history='alexnet__2022-06-23_09-16-15', split_mode=False, cls_num=7)
@@ -17,8 +17,9 @@ if __name__ == "__main__":
         weight_avg = weight_avg.cuda()
 
     if output.size(0) == weight_avg.size(0):
-        sftmx = F.softmax(output)
-        score = float((sftmx * weight_avg).sum())
+        softmax_output = F.softmax(output)
+        print(softmax_output.sum())
+        score = float((softmax_output * weight_avg).sum())
         print('Score : ' + str(round(score, 2)))
 
     else:
