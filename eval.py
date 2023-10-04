770cc84f430f9d5702077a5a585ebd7c1ee3b3b9
admin <admin@users.noreply.github.com>
2023-10-05 00:01:00 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     classes = ['PearlRiver', 'YoungChang', 'Steinway-T',
                'Hsinghai', 'Kawai', 'Steinway', 'Kawai-G', 'Yamaha']
     if classes is None:
-        ds = load_dataset("george-chou/pianos_mel")
+        ds = load_dataset("MuGeminorum/pianos_mel")
         classes = ds['test'].features['label'].names
 
     eval(classes, tag=args.target, history=args.log, split_mode=True)
